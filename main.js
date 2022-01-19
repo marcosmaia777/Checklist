3452ef97d1b1a0820404d7f42e0e747980a58c69
Marcos <marcosmaiafidelis777@outlook.com>
2022-01-19 14:16:15 -0300

    concluir tarefa

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 const criarTarefa = (evento) => {
     evento.preventDefault()  //Evitar de a página ficar atualizando automaticamente 
 
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
    
-   
-    
+   // Adicionar tarefa na lista
+    const tarefa = document.createElement('li')
     const conteudo = `<p class="content">${valor}</p>`
-
-   
+    tarefa.innerHTML = conteudo
+    lista.appendChild(tarefa)
     input.value = " "
 
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
+// Eventos
 novaTarefa.addEventListener('click', criarTarefa);
